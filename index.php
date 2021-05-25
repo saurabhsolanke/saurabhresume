90756d1643266875c18bea4a4aaabcc5ad711ec5
Saurabh Solanke <35625425+saurabhsolanke@users.noreply.github.com>
2021-05-25 18:27:44 +0530

    first

---
+++ index.php
@@ -0,0 +1,203 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
+  
+    <title>Resume</title>
+  </head>
+
+    <style>
+    
+    body{
+        background: rgb(131,58,180);
+        background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(228,228,228,1) 0%, rgba(163,166,165,1) 100%, rgba(249,249,249,1) 100%); 
+    }
+    </style>
+    
+  
+    <body>
+
+        <div class="container my-5 py-2 border" data-aos="fade-down" data-aos-duration="1000" style="box-shadow: 10px 10px 5px grey, 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)">
+            <div class="container my-5 py-2">
+                <div class="container mx-5">
+                        <div class="row">
+                            <div class="col-sm-4" data-aos="fade-down" data-aos-duration="1000">
+                                <img src="pro.jpg" alt="" style="width: 250px;">
+                                <h4>Contact</h4>
+                                <h5>+91-9028066793</h5>
+                                <h5>saurabh.solanke@gmail.com</a></h5>
+                                <h5>LinkedIn <a href="https://www.linkedin.com/in/saurabh-solanke-02534a166/">saurabh-solanke</a></h5>
+                                <h5>Github <a href="https://github.com/saurabhsolanke">saurabhsolanke</a></h5>
+                                <h6>Flat no. 20, Vivekanand Nagar, N-4 (South), CIDCO, Aurangabad.</h6><hr>
+
+                                <div class="container-fluid" data-aos="fade-down" data-aos-duration="800">
+                                <h2>Skills</h2><b><h4>Web Technologies</h4><h5></b>
+                                <li>HTML5</li>
+                                <li>CSS</li>
+                                <li>Bootstrap 4</li>
+                                <li>Php</li>
+                                <li>JavaScript</li>
+                                <li>Jquery</li>
+                                <li>SQL</li></h5><hr>
+                                </div>
+
+                                <div class="container-fluid" data-aos="fade-down" data-aos-duration="800">
+                                    <b><h4>Programming Languages</h4><h5></b>
+                                    <li>C</li>
+                                    <li>C++</li>
+                                    <li>Java</li>
+                                    <li>Python</li>
+                                    </h5><hr>
+                                </div>
+                                
+                                <div class="container-fluid" data-aos="fade-down" data-aos-duration="700">
+                                <h2>Language</h2>
+
+                                <h5>English</h5>
+                                <h5>Hindi</h5>
+                                <h5>Marathi</h5>
+                                </div>
+                            </div>
+                            
+                            <!-- Right Side  -->
+                            
+                            <div class="col-sm-8 mt-1">
+                                <h2 class="display-4">Saurabh Solanke</h2><hr>
+                                <h6><b>Objective</b>  -  To be associated with a growing organization that provides me the opportunity to leverage my skills as per the latest trends and to become a team member that progressively works towards the growth of the organization.</h6><hr>
+                                <h5><b> Education -</b></h5><hr><h6>
+
+                                <div class="container-fluid" data-aos="fade-down" data-aos-duration="700">
+                                <table class="table">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                        <th scope="col"></th>
+                                        <th scope="col">MIT-WPU School of Computer Science</th>
+                                        <th scope="col"></th>
+                                        
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                        <th scope="row">1</th>
+                                        <td>M.C.A Trimester-2</td>
+                                        <td>9.76 CGPA</td>
+                                        
+                                        </tr>
+                                        <tr>
+                                        <th scope="row">2</th>
+                                        <td>M.C.A Trimester-1</td>
+                                        <td>9.8 CGPA</td>
+                                        
+                                        </tr>
+                                    
+                                    </tbody>
+                                </table>
+
+                                <table class="table">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                        <th scope="col"></th>
+                                        <th scope="col">MGM's Dr. G.Y.Pathrikar College of CS & IT</th>
+                                        <th scope="col"></th>
+                                        
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                        <th scope="row">1</th>
+                                        <td>B.C.S T.Y 2019-2020</td>
+                                        <td>75.6%</td>
+                                        </tr>
+
+                                        <tr>
+                                        <th scope="row">2</th>
+                                        <td> B.C.S S.Y  2018-19  </td>
+                                        <td>62.2%</td>
+                                        </tr>
+
+                                        <tr>
+                                        <th scope="row">3</th>
+                                        <td>B.C.S F.Y  2017-18 </td>
+                                        <td>60.8%</td>
+                                        </tr>
+                                    
+                                    </tbody>
+                                </table>
+                                </div>
+
+                                <div class="container-fluid" data-aos="fade-down" data-aos-duration="700">
+                                <table class="table">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                        <th scope="col"></th>
+                                        <th scope="col">Shiv Chhatrapati College, Aurangabad(2015)</th>
+                                        <th scope="col"></th>
+                                        
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                        <th scope="row">1</th>
+                                        <td> 12th HSC Board </td>
+                                        <td>60.15% </td> 
+                                        </tr>
+                                    </tbody>
+                                </table>
+
+                                <table class="table">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                        <th scope="col"></th>
+                                        <th scope="col"> St.Lawrence High School Aurangabad(2013)</th>
+                                        <th scope="col"></th>
+                                        
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                        <th scope="row">1</th>
+                                        <td> 10th SSC Board</td>
+                                        <td> 53.69%</td>
+                                        </tr>
+                                    
+                                    </tbody>
+                                </table>
+                                </div>
+                                <hr>
+
+                                <div class="container-fluid" data-aos="fade-down" data-aos-duration="1000">
+                                    <h4>Projects:-</h4>
+                                    <h5>Graduation</h5> 
+                                    <h6>
+                                    <li>2nd-year Mini-Project in Visual Basic .Net ‘Bon Appetit’ Cafe billing system.</li>
+                                    <li>3rd year Project named ‘Babble’ which is a chat application for web-based on Websockets. </li></h6>
+
+                                    <h5>Post Graduation</h5>
+                                    <h6>
+                                    <li>First Trimester - Plasma Donation For Covid-19 Patients on php.</li>
+                                    <li>Second Trimester - Website Skeleton on php.</li>
+                                    <li>Third Trimester - Academic Almanac on  ASP.NET.</li></h6><hr>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+        
+    </body>
+    <script>
+            AOS.init();
+        </script>
+</html>
